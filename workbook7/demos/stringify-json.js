e88f7c8e0dcc191cd558fd75852789ad541494a9
Talib Smith <talibsmith77@gmail.com>
2023-12-04 11:15:27 -0500

    workbook 7

---
+++ workbook7/demos/stringify-json.js
@@ -0,0 +1,12 @@
+"use strict"
+
+let job = {
+    Title:"Web Designer",
+    StartDate: "October 2024",
+    company : "AT&T",
+    minSalary : 52000,
+    maxSalary : 86000
+}
+
+let jsonString = JSON.stringify(job)
+console.log(jsonString); 
